2df154274e71c851991e3146c5fc69ba0a211ee2
anondita <anondita@commutatus.com>
2019-01-08 13:41:42 +0530

    signup corrected

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,9 +11,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    
   end
+
 test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -24,6 +24,6 @@ test "valid signup information" do
     end
     follow_redirect!
     assert_template 'users/show'
-assert_not flash.FILL_IN
+   
   end
 end
