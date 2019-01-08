63f3593ad1996da269275073775e5dca4016142c
anondita <anondita@commutatus.com>
2019-01-08 17:31:06 +0530

    initial page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-get  'static_pages/about'
-  get 'sessions/new'
-  get 'users/new'
-  get 'welcome/index'
-
-resources :articles do
-  resources :comments
-end
- # root 'static_pages#home'
+  root 'welcome#index'
+get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-post '/signup',  to: 'users#create'
 get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
