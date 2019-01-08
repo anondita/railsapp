88f97613151d6219ee5b6264c9b3db99262b7610
anondita <anondita@commutatus.com>
2019-01-08 16:32:03 +0530

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+get  'static_pages/about'
   get 'sessions/new'
   get 'users/new'
   get 'welcome/index'
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
 resources :articles do
   resources :comments
 end
- root 'welcome#index'
+ # root 'static_pages#home'
   get  '/signup',  to: 'users#new'
 post '/signup',  to: 'users#create'
 get    '/login',   to: 'sessions#new'
