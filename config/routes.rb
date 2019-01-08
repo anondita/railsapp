37cb572f1024188be41e2600a7e891c9f597424f
anondita <anondita@commutatus.com>
2019-01-08 13:05:24 +0530

    signup completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get 'users/new'
   get 'welcome/index'
+
 resources :articles do
   resources :comments
 end
  root 'welcome#index'
   get  '/signup',  to: 'users#new'
+post '/signup',  to: 'users#create'
+resources :users
 end
