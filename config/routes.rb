78d05d5e53981aea8cbe1016e489c197cff9f5b5
anondita <anondita@commutatus.com>
2019-01-08 15:32:02 +0530

    initialize repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'welcome/index'
 
@@ -8,5 +9,8 @@ end
  root 'welcome#index'
   get  '/signup',  to: 'users#new'
 post '/signup',  to: 'users#create'
+get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 resources :users
 end
